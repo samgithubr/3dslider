e02463625f1c2c9041109865d1107cc0f1085e5d
Somnath Dhotre <150257803+samgithubr@users.noreply.github.com>
2024-02-10 14:08:54 +0530

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,81 @@
+ 
+
+function clic()
+{
+  
+    let a = document.getElementById('h');
+    let b=document.getElementById('in');
+    let audio = new Audio("./voice/hey_its_me_goku.mp3");
+    audio.play();
+    g=audio.play();
+    b.value= "SAIYN GOKU";
+    
+}
+function clic2()
+{
+
+    let a = document.getElementById('h2');
+    let b=document.getElementById('in');
+
+
+    let audio = new Audio("./voice/monkey_d_luffy.mp3");
+    audio.play();
+    b.value="YO. MONKEY D LUFFY";
+    
+}
+function clic3()
+{
+  
+    let a = document.getElementById('h3');
+    let b=document.getElementById('in');
+    let audio = new Audio("./voice/asta-is-my-made-with-Voicemod.mp3");
+    audio.play();
+    b.value= "LONE AASTA";
+}
+function clic4()
+{
+   
+    let a = document.getElementById('h4');
+    let b=document.getElementById('in');
+    let audio = new Audio("./voice/Voicy__Deku it is!_.mp3");
+    audio.play()
+    b.value= "ROG DEKU";
+}
+function clic5()
+{
+   
+    let a = document.getElementById('h5');
+    let b=document.getElementById('in');
+    let audio = new Audio("./voice/first-of-all-i-am-vegeta-made-with-Voicemod (1).mp3");
+    audio.play();
+    b.value= "PRINCE VEGETA";
+    
+}
+function clic6()
+{
+ 
+    let a = document.getElementById('h6');
+    let b=document.getElementById('in');
+    let audio = new Audio("./voice/uzumaki_naruto.mp3");
+    audio.play()
+    b.value= "NARUTO";
+    
+}
+function clic7()
+{
+  
+    let a = document.getElementById('h7');
+    let b=document.getElementById('in');
+    let audio = new Audio("./voice/I-dont-believe-it-Kidding-its-just-as-I-expected-raw.mp3");
+    audio.play()
+    b.value= "BROKEN HERO OBITO";
+}
+function clic8()
+{
+ 
+    let a = document.getElementById('h8');
+    let b=document.getElementById('in');
+    let audio = new Audio("./voice/roronoa-zoro-voice-say-his-name.mp3");
+    audio.play()
+    b.value= "ZORO DA.";
+}
